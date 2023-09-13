708ac64fc817e3270c2b06e9ff32200067fded83
isabella <isanjenga9@gmail.com>
2023-09-13 10:45:49 +0300

    initial commit

---
+++ challange1.js
@@ -0,0 +1,10 @@
+let length = 12;
+let width = 10;
+
+let area = length * width;
+
+console.log(area);
+
+let perimeter = (length + width) * 2
+
+console.log(perimeter);
---
+++ challange2.js
@@ -0,0 +1,15 @@
+let n1= 300
+let n2 = 400
+
+let sum = n1+n2 // addition
+let diff = n2-n1 // subtraction
+let product = n1*n2 // multiplication
+let division = n2/n1 // division
+let modulus = n2%n1 // modulus
+
+
+console.log(sum);
+console.log(diff);
+console.log(product);
+console.log(division);
+console.log(modulus);
