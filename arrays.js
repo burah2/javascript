f6ffc4e33b04f8a91974b55861eeae01ab966131
isabella <isanjenga9@gmail.com>
2023-09-14 10:19:45 +0300

    changes done successfully

---
+++ arrays.js
@@ -0,0 +1,15 @@
+let numbers=[1,2,3,4,5,6,];
+let cars=["BMW","TOYOTA"];
+let mixed =[[1,2,3,4,5],["BMW","TOYOTA"],["dogs","cats","goats"]];
+
+console.log(mixed[2][1]);
+
+
+numbers.push("7");
+console.log(numbers);
+
+
+
+let fruits = ['apple', 'banana', 'orange'];
+fruits.push('mango');
+console.log(fruits); 
---
+++ control_structures.js
@@ -0,0 +1,12 @@
+let age= 30;
+
+if (age >18) {
+    console.log("eligible to vote");
+}
+
+let name = "john";
+ if (name= "isabella");
+
+ else;
+ console.log( "your name is not john")
+}
